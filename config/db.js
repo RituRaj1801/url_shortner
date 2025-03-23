835293ad1f623cebe0a0f68689251ef0c2cd0182
rituraj <rituraj995kumar@gmail.com>
2025-03-23 18:02:37 +0530

    .env.example

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: process.env.DB_NAME,  // ✅ Correct way to specify the database
+            dbName: process.env.DB_NAME,  
         });
         console.log("✅ MongoDB Connected!");
     } catch (error) {
--- server.js
+++ server.js
@@ -2,41 +2,32 @@ import express from "express";
 import { join } from "path";
 import connectDB from "./config/db.js";
 import shortenRoutes from "./routes/shorten.js";
-import ShortURL from "./models/ShortURL.js"; // ✅ Import the ShortURL model
+import ShortURL from "./models/ShortURL.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB 
 connectDB();
 
 app.use(express.json());
     
-// serve template filte start
 app.set("view engine", "ejs");
 app.set("views", join(process.cwd(), "public", "view"));
 app.get('/template', (req, res) => {
     res.render("template",{'html': 'Hello World!'});
 })
-// server template file end
-
-// Serve HTML file
 app.get("/", (req, res) => {
     res.sendFile(join(process.cwd(), "public", "view", "url_shortner.html"));
 });
-
-// Shorten URL Routes
-// Shorten URL Routes
 app.use("/api", shortenRoutes);
 
-// Redirect Short URL to Long URL
 app.get('/:shortCode', async (req, res) => {
     try {
         const shortCode = req.params.shortCode;
         const urlEntry = await ShortURL.findOne({ short_code: shortCode });
 
         if (urlEntry) {
-            return res.redirect(urlEntry.long_url); // ✅ Redirect to the original URL
+            return res.redirect(urlEntry.long_url); 
         } else {
             return res.status(404).send("URL not found!");
         }
@@ -46,7 +37,6 @@ app.get('/:shortCode', async (req, res) => {
     }
 });
 
-// Move this line to the bottom
 app.listen(PORT, () => console.log(`🚀 Server started on http://localhost:${PORT}`));
 app.use(express.static("public"));
 
