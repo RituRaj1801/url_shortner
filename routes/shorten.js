a426c2c2b0c6970f109235c7fdd4b44fb90b84d0
rituraj <rituraj995kumar@gmail.com>
2025-03-24 23:29:47 +0530

    exitsing link

--- routes/shorten.js
+++ routes/shorten.js
@@ -9,17 +9,38 @@ router.post("/shorten", async (req, res) => {
         if (!long_url) return res.status(400).json({ status: false, message: "URL is required!" });
 
         const url = new URL(long_url);
+
+        const existingUrl = await ShortURL.findOne({ long_url: url.href });
+
+        if (existingUrl) {
+            return res.json({
+                status: true,
+                short_url: existingUrl.short_url,
+                message: "URL already shortened!"
+            });
+        }
+
         const shortCode = Math.random().toString(36).substring(2, 8);
         const shortUrl = `${req.protocol}://${req.get("host")}/${shortCode}`;
 
-        const newShortUrl = new ShortURL({ long_url: url.href, short_code: shortCode, short_url: shortUrl });
+        const newShortUrl = new ShortURL({
+            long_url: url.href,
+            short_code: shortCode,
+            short_url: shortUrl
+        });
+
         await newShortUrl.save();
 
-        return res.json({ status: true, short_url: shortUrl, message: "URL shortened successfully!" });
+        return res.json({
+            status: true,
+            short_url: shortUrl,
+            message: "URL shortened successfully!"
+        });
 
     } catch (error) {
         res.status(504).json({ status: false, message: error.message || "Request timed out" });
     }
+
 });
 
 
